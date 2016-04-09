3b51dca98d19246d8a27b7de330b9df794cae12d
Samuel Gui <samy_gui95@yahoo.com>
2016-04-09 12:30:22 +0300

    Initial commit

---
+++ src/P2.java
@@ -0,0 +1,16 @@
+public class P2 {
+
+    public static void main(String[] args) {
+        int n=20;
+        int sum=0;
+            for(int i=0;i<=n;i++)
+            {
+                sum+=i;
+
+            }
+        System.out.println("Suma primelor 20 numere este:"+sum);
+
+
+
+    }
+}
